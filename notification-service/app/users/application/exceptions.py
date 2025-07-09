a5cc8bd951af7bfa04cd0594c4b738f8ca032e39
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-07-09 13:57:23 -0600

    add user module

---
+++ notification-service/app/users/application/exceptions.py
@@ -0,0 +1,10 @@
+class AuthenticationError(Exception):
+    """Custom exception for authentication errors"""
+
+    pass
+
+
+class AuthorizationError(Exception):
+    """Custom exception for authorization errors"""
+
+    pass
