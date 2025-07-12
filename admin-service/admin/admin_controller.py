dbbe542d22f2a506e08256fe5a9bbc234a1eb1c7
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-07-11 22:45:46 -0600

    Admin Service Refactor

--- admin-service/admin/admin_controller.py
+++ admin-service/admin/admin_controller.py
@@ -1,97 +1,64 @@
 from fastapi import APIRouter, HTTPException, status
 from datetime import datetime
 from pydantic import BaseModel
-from .admin import registered_services, ServiceRegistration
+from .admin_service import ServiceRegistration, RegistryService
+import logging
 
-app = APIRouter(prefix="/api/v2/admin-manager")
+logger = logging.getLogger(__name__)
+
+router = APIRouter(prefix="/api/v2/admin-manager")
+service_registry = RegistryService()
 
 
-@app.post("/register-service", status_code=status.HTTP_201_CREATED)
+@router.post("/register-service", status_code=status.HTTP_201_CREATED)
 async def register_service(registration: ServiceRegistration):
     """
-    Endpoint para que los microservicios se registren.
-    Genera un ID de instancia único si no se proporciona.
+    Endpoint for microservices to register.
+    Generates a unique instance ID if one is not provided.
     """
-    instance_id = f"{registration.service_name}-{registration.host}-{registration.port}"
-
-    if registration.service_name not in registered_services:
-        registered_services[registration.service_name] = {}
-
-    registered_services[registration.service_name][instance_id] = {
-        "host": registration.host,
-        "port": registration.port,
-        "health_check_endpoint": registration.health_check_endpoint,
-        "last_heartbeat": datetime.now(),
-        "status": "UNKNOWN",  # Estado inicial
-    }
-    print(f"Service '{registration.service_name}' registrado: {instance_id}")
+    instance_id = service_registry.register_service(registration)
+    logger.info(
+        f"Service '{registration.service_name}' registered with instance ID: {instance_id}"
+    )
     return {"message": "Service registered successfully", "instance_id": instance_id}
 
 
-@app.post("/heartbeat/{service_name}/{instance_id}")
-async def receive_heartbeat(service_name: str, instance_id: str):
+@router.post("/heartbeat/{instance_id}")
+async def receive_heartbeat(instance_id: str):
     """
-    Endpoint para que los servicios envíen un heartbeat.
-    Actualiza el timestamp del último heartbeat.
+    Endpoint for services to send a heartbeat.
+    Updates the last heartbeat timestamp.
     """
-    if (
-        service_name in registered_services
-        and instance_id in registered_services[service_name]
-    ):
-        registered_services[service_name][instance_id][
-            "last_heartbeat"
-        ] = datetime.now()
-        print(f"Heartbeat recibido de {service_name} ({instance_id})")
-        return {"message": "Heartbeat received"}
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="Service instance not found"
-    )
+    if not service_registry.update_heartbeat(instance_id):
+        logger.warning(f"Heartbeat received for unknown instance ID: {instance_id}")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Service instance not found"
+        )
+    logger.debug(f"Heartbeat received for instance ID: {instance_id}")
+    return {"message": "Heartbeat received"}
 
 
-@app.get("/available-services")
+@router.get("/available-services")
 async def get_available_services():
     """
-    Endpoint para obtener una lista de todos los servicios registrados y su estado.
+    Endpoint to get a list of all registered services and their status.
     """
-    response_data = []
-    for service_name, instances in registered_services.items():
-        for instance_id, data in instances.items():
-            response_data.append(
-                {
-                    "service_name": service_name,
-                    "instance_id": instance_id,
-                    "host": data["host"],
-                    "port": data["port"],
-                    "status": data["status"],
-                    "last_heartbeat": data["last_heartbeat"].isoformat(),
-                }
-            )
-    return {"services": response_data}
+    services = service_registry.get_all_services()
+    logger.debug("Retrieving all available services.")
+    return {"services": services}
 
 
-@app.get("/available-services/{service_name}")
+@router.get("/available-services/{service_name}")
 async def get_service_instances(service_name: str):
     """
-    Endpoint para obtener instancias de un servicio específico.
+    Endpoint to get instances of a specific service.
     """
-    if service_name in registered_services:
-        instances_list = []
-        for instance_id, data in registered_services[service_name].items():
-            if data["status"] == "UP":
-                instances_list.append(
-                    {
-                        "instance_id": instance_id,
-                        "host": data["host"],
-                        "port": data["port"],
-                    }
-                )
-        if not instances_list:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"No UP instances found for service {service_name}",
-            )
-        return {"service_name": service_name, "instances": instances_list}
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Service '{service_name}' not found",
-    )
+    instances = service_registry.get_service_instances(service_name)
+    if not instances:
+        logger.warning(f"No instances found for service: {service_name}")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No instances found for service {service_name}",
+        )
+    logger.debug(f"Retrieving instances for service: {service_name}")
+    return {"service_name": service_name, "instances": instances}
--- admin-service/main.py
+++ admin-service/main.py
@@ -5,29 +5,42 @@ from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 from contextlib import asynccontextmanager
 import aiohttp
-from admin.admin import registered_services
+from admin.admin_service import RegistryService
+from admin.health_monitor import HealthMonitor
 from admin import admin_controller
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
-# Admin config
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Admin Service iniciado. Activando monitor de salud...")
-    health_monitor_task = asyncio.create_task(health_check_monitor())
+    logger.info("Admin Service started. Activating health monitor...")
+    service_registry = RegistryService()
+    health_monitor = HealthMonitor(service_registry)
+
+    monitor_task = asyncio.create_task(health_monitor.start_monitoring())
     yield
-    print("Admin Service cerrando. Cancelando tarea de monitor de salud.")
-    health_monitor_task.cancel()
+    logger.info("Admin Service shutting down. Cancelling health monitor task.")
+
+    await health_monitor.stop_monitoring()
+    monitor_task.cancel()
+
     try:
-        await health_monitor_task
+        await monitor_task
     except asyncio.CancelledError:
-        print("Tarea de monitor de salud cancelada exitosamente.")
+        pass
+        logger.info("Health monitor task cancelled successfully.")
 
 
 app = FastAPI(
     title="Admin Service - Service Registry & Health Dashboard",
-    description="Servicio para registrar microservicios y monitorear su salud.",
+    description="Service for registering microservices and monitoring their health. Also for setting up queues and exchanges for RabbitMQ.",
     lifespan=lifespan,
 )
 
@@ -43,43 +56,6 @@ app.add_middleware(
 )
 
 
-async def perform_health_check(service_data: dict, instance_id: str):
-    """Realiza una llamada de salud a un servicio."""
-    url = f"http://{service_data['host']}:{service_data['port']}{service_data['health_check_endpoint']}"
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                url, timeout=aiohttp.ClientTimeout(total=5)
-            ) as response:
-                if response.status == 200:
-                    registered_services[service_data["service_name"]][instance_id][
-                        "status"
-                    ] = "UP"
-                else:
-                    registered_services[service_data["service_name"]][instance_id][
-                        "status"
-                    ] = "DOWN"
-                print(
-                    f"Health check for {instance_id}: {response.status} -> {registered_services[service_data['service_name']][instance_id]['status']}"
-                )
-    except Exception as e:
-        registered_services[service_data["service_name"]][instance_id][
-            "status"
-        ] = "DOWN"
-        print(f"Health check for {instance_id} failed: {e} -> DOWN")
-
-
-async def health_check_monitor():
-    """Tarea en segundo plano para monitorear la salud de los servicios."""
-    while True:
-        if asyncio.current_task().done():
-            break
-        for service_name, instances in registered_services.items():
-            for instance_id, data in instances.items():
-                await perform_health_check(data, instance_id)
-        await asyncio.sleep(10)
-
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -90,8 +66,7 @@ async def get_dashboard():
     return HTMLResponse(content=html_content)
 
 
-app.include_router(admin_controller.app)
+app.include_router(admin_controller.router)
 
 if __name__ == "__main__":
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
