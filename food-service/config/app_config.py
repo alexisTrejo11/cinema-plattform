b89c3d3e7110c0bb51dfeba51d64b87d918e2629
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-07-21 22:39:40 -0600

    loggers and registry service with admin_service

---
+++ food-service/config/app_config.py
@@ -0,0 +1,47 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    # Application Configuration
+    API_NAME: str = "Product Service"
+    API_HOST: str
+    API_PORT: int
+    API_VERSION: str = "2.0"
+    DEBUG_MODE: bool = False
+
+    REGISTRY_ADMIN_URL: str
+
+    # JWT Configuration
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+
+    # Database Configuration
+    DATABASE_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    # RabbitMQ Configuration
+    # RABBITMQ_URL: str
+    # USER_EVENTS_EXCHANGE: str
+    # CONSUMER_QUEUE_NAME: str
+    # WALLET_EXCHANGE: str
+
+    model_config = SettingsConfigDict(
+        env_file="./.env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+@lru_cache()
+def get_settings_cached_instance():
+    """
+    Returns a cached instance of the Settings.
+    This ensures the .env file is read only once.
+    """
+    return Settings()  # type: ignore
+
+
+settings = get_settings_cached_instance()
